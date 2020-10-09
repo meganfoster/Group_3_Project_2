6d70507e40b13683d3e387de97df0f4f138e039e
Kevin Avery <Averykev@gmail.com>
2020-10-08 22:00:11 -0700

    updates to heatmap & index.html

--- assets/js/heatmap.js
+++ assets/js/heatmap.js
@@ -1,6 +1,6 @@
 var myMap = L.map("map", {
-    center: [37.7749, -122.4194],
-    zoom: 13
+    center: [39.000, -98.000],
+    zoom: 5
   });
   
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -8,29 +8,29 @@ var myMap = L.map("map", {
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
+    id: "mapbox/light-v10",
+    accessToken: "pk.eyJ1IjoiYXZlcnlrZXYiLCJhIjoiY2tmb3E2M2p1MDJldTJxbXIzbThnODVjZyJ9.pIvdoYlUD16lir88dz7dIg"
   }).addTo(myMap);
   
-  var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
+var sleepData = "assets/data/500_Cities__Sleeping_less_than_7_hours_among_adults_aged___18_years.csv";
   
-  d3.json(url, function(response) {
+  d3.json(sleepData, function(response) {
   
-    console.log(response);
+    console.log(response);})
   
-    var heatArray = [];
+//     var heatArray = [];
   
-    for (var i = 0; i < response.length; i++) {
-      var location = response[i].location;
+//     for (var i = 0; i < response.length; i++) {
+//       var location = response[i].location;
   
-      if (location) {
-        heatArray.push([location.coordinates[1], location.coordinates[0]]);
-      }
-    }
+//       if (location) {
+//         heatArray.push([location.coordinates[1], location.coordinates[0]]);
+//       }
+//     }
   
-    var heat = L.heatLayer(heatArray, {
-      radius: 20,
-      blur: 35
-    }).addTo(myMap);
+//     var heat = L.heatLayer(heatArray, {
+//       radius: 20,
+//       blur: 35
+//     }).addTo(myMap);
   
-  });
+//   })
